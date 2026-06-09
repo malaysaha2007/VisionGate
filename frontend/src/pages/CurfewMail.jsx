e3e0dcbe0600c4be87e6124ea1c15ebd6404c5a2
Aditichouhan54 <aditichouhan54@gmail.com>
2026-06-09 09:51:02 +0530

    Moved Refresh and Send Mail buttons back to where it was

--- frontend/src/pages/CurfewMail.jsx
+++ frontend/src/pages/CurfewMail.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 import {
   FaSyncAlt,
